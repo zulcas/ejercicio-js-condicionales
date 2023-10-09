d579e0070a616e4f45bdac3846389b765a3bfe57
omiras <oscar.miras@gmail.com>
2023-10-09 08:42:56 +0200

    fix ex9

--- ex9.js
+++ ex9.js
@@ -6,7 +6,7 @@
  * 1. Modifica el valor de las variables adecuadamente para que se muestre el mensaje por consola "Yo soy de Barcelona, pero no tengo una segunda residencia en Platja d'aro..."
  */
 
-let soyDeBarcelona = true;
+let soyDeBarcelona;
 let tengoSegundaResidencia = true;
 
 /** NO TOCAR A PARTIR DE AQUÍ */
